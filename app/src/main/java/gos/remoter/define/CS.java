88f57d07ed651853b3084dcf7492d1e4bd8235f8
QXTX_GOSDOWS10 <1085466442@qq.com>
2017-09-05 15:31:03 +0800

    创建了EPG的j静态常量类。gos.remoter.define.CS.class

---
+++ app/src/main/java/gos/remoter/define/CS.java
@@ -0,0 +1,12 @@
+package gos.remoter.define;
+
+/**
+ * Created by QXTX-GOSPELL on 2017/9/5 0005.
+ * 静态常量
+ */
+
+public class CS {
+    public static final String EPG_TAG = "来自EPGACT的消息";
+    public static final String EPG_ONCREATE = "进入EPGACT的onCreate()方法";
+    public static final String EPG_ONDESTROY = "进入EPGACT的onDwestroy()方法";
+}

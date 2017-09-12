b83eab91a281b145d86dbf65070a0eb19e3fe145
QXTX_GOSDOWS10 <1085466442@qq.com>
2017-09-12 10:35:20 +0800

    无影响修改，补充了adapter的注释 Epg_myAdapter.class

--- app/src/main/java/gos/remoter/adapter/Epg_myAdapter.java
+++ app/src/main/java/gos/remoter/adapter/Epg_myAdapter.java
@@ -20,29 +20,42 @@ import gos.remoter.R;
 
 /**一个通配适配器
  * 传参：
- * 1、上下文
- * 2、item
- * 3、布局id
+ * 1、上下文：为了寻找到布局的id
+ * 2、item：传入一个条目的所有数据集合
+ * 3、布局id：为了寻找到指定的layout布局
  *
  * 备注：ViewGroup:上层layout， 利用它可以对上层layout进行操作
  */
 
 public abstract class Epg_myAdapter<T> extends BaseAdapter {
-    private Context context;
-    private ArrayList<T> list;
+    private Context context;//这里保存是为了给内部类也能使用上下文
+    private ArrayList<T> list;//一个通用item列表，将获取到的item全部保存到这里，组成一个listview
     private int layoutRes;//布局的id需要从外面传进来，bind中需要用它来获取convertView
 
+    //构造方法，从ACT中传入必需的三个数据
     public Epg_myAdapter(Context context, ArrayList<T> list, int layoutRes) {
         this.context = context;
         this.list = list;
         this.layoutRes = layoutRes;
     }
 
+    /**Adapter中总是第一个被调用的方法
+     * 功能：
+     * 1、每一次列表被更新，都会执行一次getCount()方法，为了计算出列表中一共有多少个item条目
+     */
     @Override
     public int getCount() {
         //Log.e("消息", "一共有" + list.size() + "行");
         return list == null? 0 : list.size();
     }
+
+    /**getItem()和getItemId()两个方法
+     * 用途：
+     * 1、getItem()方法用于获得某个位置上的item数据，返回一个通用的view，可能需要类型转换
+     * 2、getItemId()方法用于获得某个位置上的item的id，返回long类型的id，需要类型转换（转成int）
+     *
+     * 备注：它们只有在被要求时才会被调用
+     */
     @Override
     public T getItem(int position) {
         return list.get(position);
@@ -51,6 +64,20 @@ public abstract class Epg_myAdapter<T> extends BaseAdapter {
     public long getItemId(int position) {
         return position;
     }
+
+    /**getView()方法
+     *
+     * 这个方法在每次获取到一个item，都会被调用一次
+     * 此方法提供item自动复用机制
+     * 本方法中的convertView有一下特点：
+     *   1、一个屏幕中可以显示n个item，那么前n+2次调用getVIew()方法时convertView都是null；（固定缓存n+2个convertView）
+     *   2、当屏幕上的列表被滚动，当第n+3个item出现在屏幕中，这个item会由于convertView的复用机制，被填充第1个item的数据；
+     *   也就是第一个item的convertView被回收，给了第n+3个item。
+     *   3、如果给第1个item设置了Tag,则当列表回滚第1个Item再次出现在屏幕上，这个item将会直接通过Tag得到已保存的数据，
+     *   不需要再次申请，而屏幕外的第n+2之后的item全部被回收。
+     *
+     * 备注：由于每次更新列表都会导致getView被重新调用，因此需要注意其中的局部变量将会被反复初始化而不能长时保存数据。
+     */
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         /**将获取convertView部分放到Holder构造中
@@ -70,7 +97,8 @@ public abstract class Epg_myAdapter<T> extends BaseAdapter {
 
 
 /************************静态内部类Holder*****************************/
-    /**静态的内部类
+
+    /**静态的内部类Holder
      *
      * 这里的Holder需要完成的工作：
      * 1、保存convertView
@@ -79,7 +107,8 @@ public abstract class Epg_myAdapter<T> extends BaseAdapter {
      *
      * 备注1：暴露的set方法，用于在Adapter外部也能实现view属性的设置
      * 备注2：因为这里的Adapter为泛型适配器，传进来的类型千奇百怪，无法在这里进行预先设置，所以需要暴露set方法以便在外部设置
-     */
+     * 备注3：关于为什么设置为静态，是为了避免反复调用时被重复创建，增加浪费。如果只调用一次，可以不需要设置为静态
+     * */
     public static class Holder {
         private int position;
         private View convertView;//保存主view
@@ -114,7 +143,6 @@ public abstract class Epg_myAdapter<T> extends BaseAdapter {
                                   ViewGroup parent, int layoutRes, Context context) {
             Holder holder = null;
             if (convertView == null) {
-                //Log.e("消息", "convertView为空");
                 holder = new Holder(context, parent, layoutRes);//这里完成了holder的两项基本工作
             } else {
                 //从convertView中获取到holder
@@ -127,6 +155,7 @@ public abstract class Epg_myAdapter<T> extends BaseAdapter {
 
 
 /**************实例化item的各种属性、获取item位置*******************/
+
         /**id实例化，保存得到的view
          *
          * 1、将获取到的各种view的id进行实例化，以便提供给暴露的set方法使用
@@ -157,6 +186,7 @@ public abstract class Epg_myAdapter<T> extends BaseAdapter {
 
 
 /*******************实例化item中的各种属性************************/
+
         /**上文获取了一堆id，这里将要set一堆数据
          *
          * 目前set方法有
@@ -258,6 +288,7 @@ public abstract class Epg_myAdapter<T> extends BaseAdapter {
 
 
 /*******************为Adapter暴露添加元素的方法************************/
+
     /**这里是Adapter方法
      * 目前可暴露的方法有：
      * 1、添加一个item（默认在队列末端）
@@ -284,6 +315,7 @@ public abstract class Epg_myAdapter<T> extends BaseAdapter {
 
 
 /*************************抽象方法*****************************/
+
     /**抽象方法，
      * 1、为不同蜡yout设置不同的数据
      * 2、实现暴露的方法
